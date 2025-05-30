0dbb7cc7a4165edbe83e7b6bd119ff610e01ce18
Raheem-cm <nyoniraheem@gmail.com>
2025-05-30 15:02:03 +0300

    Raheem.files

---
+++ RAHEEM-XMD.1_files/config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  owner: ['255763111390'],
+  channel: 'https://whatsapp.com/channel/0029VbAffhD2ZjChG9DX922r',
+  menuAudio: './media/menu.mp3',
+  sessionAudio: './media/session-id.mp3'
+};
---
+++ RAHEEM-XMD.1_files/index.js
@@ -0,0 +1,4 @@
+
+const fs = require('fs');
+const { exec } = require('child_process');
+console.log("RAHEEM-XMD.1 BOT STARTED");
